fff081732096e887e180b72c695b9152f2716cf4
Madrajib LAB <madhabsharma94@gmail.com>
2024-12-07 17:54:43 +0530

    Circular Progress Bar and Updated print screen

--- CYD-Klipper/src/ui/panels/progress_panel.cpp
+++ CYD-Klipper/src/ui/panels/progress_panel.cpp
@@ -18,6 +18,11 @@ static void progress_bar_update(lv_event_t* e){
     lv_bar_set_value(bar, printer.print_progress * 100, LV_ANIM_ON);
 }
 
+static void progress_arc_update(lv_event_t* e){
+    lv_obj_t * arc = lv_event_get_target(e);
+    lv_arc_set_value(arc, printer.print_progress * 360);
+}
+
 static void update_printer_data_elapsed_time(lv_event_t * e){
     lv_obj_t * label = lv_event_get_target(e);
     lv_label_set_text(label, time_display(printer.elapsed_time_s));
@@ -55,6 +60,27 @@ static void update_printer_data_percentage(lv_event_t * e){
     char percentage_buffer[12];
     sprintf(percentage_buffer, "%.2f%%", printer.print_progress * 100);
     lv_label_set_text(label, percentage_buffer);
+}
+
+static void update_printer_layer_printed(lv_event_t * e){
+    lv_obj_t * label = lv_event_get_target(e);
+    char layer_buffer[12];
+    sprintf(layer_buffer, "%d/%d", printer.current_layer , printer.total_layers );
+    lv_label_set_text(label, layer_buffer);
+}
+
+static void update_printer_hotend(lv_event_t * e){
+    lv_obj_t * label = lv_event_get_target(e);
+    char layer_buffer[20];
+    snprintf(layer_buffer, sizeof(layer_buffer), "%3.1f/%3.0f", printer.extruder_temp, printer.extruder_target_temp);
+    lv_label_set_text(label, layer_buffer);
+}
+
+static void update_printer_bed(lv_event_t * e){
+    lv_obj_t * label = lv_event_get_target(e);
+    char layer_buffer[20];
+    snprintf(layer_buffer, sizeof(layer_buffer), "%3.1f/%3.0f", printer.bed_temp , printer.bed_target_temp );
+    lv_label_set_text(label, layer_buffer);
 }
 
 static void btn_click_stop(lv_event_t * e){
@@ -106,42 +132,120 @@ void progress_panel_init(lv_obj_t* panel){
         lv_obj_align(center_panel, LV_ALIGN_CENTER, 0, 0);
     }
 
+    lv_obj_t * top_panel = lv_create_empty_panel(center_panel);
+    lv_obj_set_size(top_panel, panel_width, LV_SIZE_CONTENT);
+    lv_obj_set_style_pad_top(top_panel, 0, LV_PART_MAIN);
+    lv_obj_set_style_pad_bottom(top_panel, 10, LV_PART_MAIN);
+    lv_obj_clear_flag(top_panel, LV_OBJ_FLAG_SCROLLABLE);
+    lv_layout_flex_row(top_panel);
+    
+    lv_obj_set_flex_align(top_panel, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_START);
+
     // Filename
-    lv_obj_t * label = lv_label_create(center_panel);
+    lv_obj_t * label = lv_label_create(top_panel);
     lv_label_set_text(label, printer.print_filename);
     if (global_config.full_filenames) lv_label_set_long_mode(label, LV_LABEL_LONG_WRAP);
     else lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR);
     lv_obj_set_width(label, panel_width);
-    
-    // Progress Bar
-    lv_obj_t * bar = lv_bar_create(center_panel);
-    lv_obj_set_size(bar, panel_width, CYD_SCREEN_MIN_BUTTON_HEIGHT_PX * 0.75f);
-    lv_obj_add_event_cb(bar, progress_bar_update, LV_EVENT_MSG_RECEIVED, NULL);
-    lv_msg_subsribe_obj(DATA_PRINTER_DATA, bar, NULL);
+    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
 
-    // Time
-    lv_obj_t * time_est_panel = lv_create_empty_panel(center_panel);
-    lv_obj_set_size(time_est_panel, panel_width, LV_SIZE_CONTENT);
+    lv_obj_t * mid_panel = lv_create_empty_panel(center_panel);
+    lv_obj_set_size(mid_panel, panel_width, LV_SIZE_CONTENT);
+    lv_obj_clear_flag(mid_panel, LV_OBJ_FLAG_SCROLLABLE);
+    lv_layout_flex_row(mid_panel);
+    lv_obj_set_flex_align(mid_panel, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START, LV_FLEX_ALIGN_CENTER);
+
+    lv_obj_t * left_mid_panel = lv_create_empty_panel(mid_panel);
+    lv_obj_set_size(left_mid_panel, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_align(left_mid_panel, LV_ALIGN_CENTER, 0, 0);
+
+    lv_obj_t * arc = lv_arc_create(left_mid_panel);
+    lv_obj_align(arc, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_arc_set_rotation(arc, 270);
+    lv_arc_set_bg_angles(arc, 0, 360);
+    lv_arc_set_range(arc, 0, 360);
+    lv_obj_remove_style(arc, NULL, LV_PART_KNOB);   /*Be sure the knob is not displayed*/
+    lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_add_event_cb(arc, progress_arc_update, LV_EVENT_MSG_RECEIVED, NULL);
+    lv_msg_subsribe_obj(DATA_PRINTER_DATA, arc, NULL);
+
+    lv_obj_t * right_mid_panel = lv_create_empty_panel(mid_panel);
+    // lv_obj_set_size(right_mid_panel, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_set_flex_grow(right_mid_panel, 1);
+    lv_obj_align(left_mid_panel, LV_ALIGN_RIGHT_MID, 0, 0);
+    lv_layout_flex_row(right_mid_panel);
+    lv_obj_set_flex_align(right_mid_panel, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START);
+
+    lv_obj_t * text_panel = lv_create_empty_panel(right_mid_panel);
+    lv_obj_align(text_panel, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_size(text_panel, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_layout_flex_column(text_panel);
+
+    lv_obj_t * time_panel = lv_create_empty_panel(right_mid_panel);
+    lv_obj_align(time_panel, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_set_size(time_panel, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_layout_flex_column(time_panel);
+
+    label = lv_label_create(text_panel);
+    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_label_set_text(label, "Total:");
+
+    label = lv_label_create(text_panel);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_label_set_text(label, "Rem:");
+
+    label = lv_label_create(text_panel);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_label_set_text(label, "Nozel:");
+
+    label = lv_label_create(text_panel);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_label_set_text(label, "Bed:");
 
     // Elapsed Time
-    label = lv_label_create(time_est_panel);
+    label = lv_label_create(time_panel);
     lv_label_set_text(label, "???");
-    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
     lv_obj_add_event_cb(label, update_printer_data_elapsed_time, LV_EVENT_MSG_RECEIVED, NULL);
     lv_msg_subsribe_obj(DATA_PRINTER_DATA, label, NULL);
 
     // Remaining Time
-    label = lv_label_create(time_est_panel);
+    label = lv_label_create(time_panel);
     lv_label_set_text(label, "???");
-    lv_obj_align(label, LV_ALIGN_RIGHT_MID, 0, 0);
+    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
     lv_obj_add_event_cb(label, update_printer_data_remaining_time, LV_EVENT_MSG_RECEIVED, NULL);
     lv_msg_subsribe_obj(DATA_PRINTER_DATA, label, NULL);
 
+    // Remaining Time
+    label = lv_label_create(time_panel);
+    lv_label_set_text(label, "???");
+    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_add_event_cb(label, update_printer_hotend, LV_EVENT_MSG_RECEIVED, NULL);
+    lv_msg_subsribe_obj(DATA_PRINTER_DATA, label, NULL);
+
+    // Remaining Time
+    label = lv_label_create(time_panel);
+    lv_label_set_text(label, "???");
+    lv_obj_align(label, LV_ALIGN_LEFT_MID, 0, 0);
+    lv_obj_add_event_cb(label, update_printer_bed, LV_EVENT_MSG_RECEIVED, NULL);
+    lv_msg_subsribe_obj(DATA_PRINTER_DATA, label, NULL);
+
+    lv_obj_t * arc_mid_panel = lv_create_empty_panel(arc);
+    lv_obj_set_size(arc_mid_panel, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_align(arc_mid_panel, LV_ALIGN_CENTER, 0, 0);
+    lv_layout_flex_column(arc_mid_panel);
+
     // Percentage
-    label = lv_label_create(time_est_panel);
+    label = lv_label_create(arc_mid_panel);
     lv_label_set_text(label, "???");
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
     lv_obj_add_event_cb(label, update_printer_data_percentage, LV_EVENT_MSG_RECEIVED, NULL);
+    lv_msg_subsribe_obj(DATA_PRINTER_DATA, label, NULL);
+
+    label = lv_label_create(arc_mid_panel);
+    lv_label_set_text(label, "???");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_event_cb(label, update_printer_layer_printed, LV_EVENT_MSG_RECEIVED, NULL);
     lv_msg_subsribe_obj(DATA_PRINTER_DATA, label, NULL);
 
     // Stop Button
